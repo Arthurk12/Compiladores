b929f985b6dc7bf686c2d7e7313aba117e50dd40
Unknown <ianfschilling@gmail.com>
2019-05-25 14:18:35 -0300

    Small changes in semantic.c

--- Etapa4/semantic.c
+++ Etapa4/semantic.c
@@ -2,40 +2,16 @@
 #include "semantic.h"
 
 void setDeclaration(AST *node){
-
     if(node == 0) return;
 
     int i;
-
-
     for(i=0; i<MAX_SONS; i++){
         setDeclaration(node->son[i]);
     }
 
     switch(node->type){
-
         case AST_VAR_DECLARATION:
-                                    if(node->symbol == 0) return;
-
-                                    if(node->son[0]->type == AST_DATATYPE_BYTE)
-                                        node->symbol->datatype = DATATYPE_BYTE;
-                                    else if(node->son[0]->type == AST_DATATYPE_INT)
-                                        node->symbol->datatype = DATATYPE_INT;
-                                    else if(node->son[0]->type == AST_DATATYPE_FLOAT)
-                                        node->symbol->datatype = DATATYPE_FLOAT;
-
-                                break;
         case AST_VEC_DECLARATION:
-                                    if(node->symbol == 0) return;
-
-                                    if(node->son[0]->type == AST_DATATYPE_BYTE)
-                                        node->symbol->datatype = DATATYPE_BYTE;
-                                    else if(node->son[0]->type == AST_DATATYPE_INT)
-                                        node->symbol->datatype = DATATYPE_INT;
-                                    else if(node->son[0]->type == AST_DATATYPE_FLOAT)
-                                        node->symbol->datatype = DATATYPE_FLOAT;
-
-                                break;
         case AST_VEC_DECLARATION_INI:
                                     if(node->symbol == 0) return;
 
@@ -45,10 +21,10 @@ void setDeclaration(AST *node){
                                         node->symbol->datatype = DATATYPE_INT;
                                     else if(node->son[0]->type == AST_DATATYPE_FLOAT)
                                         node->symbol->datatype = DATATYPE_FLOAT;
-                                        
+
                                 break;
-
-
+        default:
+                                break;
     }
 
 }
