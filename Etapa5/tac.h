2ceb78e6991ce9bcf2bdbccf8733ae0aebd4daa9
ifschilling <ianfschilling@gmail.com>
2019-06-14 15:43:07 -0300

    TAC type created

---
+++ Etapa5/tac.h
@@ -0,0 +1,18 @@
+/*
+ARTHUR BÖCKMANN GROSSI - 00275607
+IAN FISCHER SCHILLING - 00275603
+*/
+
+#ifndef AST_HEADER
+#define AST_HEADER
+
+#include <stdio.h>
+#include "hash.h"
+
+typedef struct tac{
+    char code;
+    hashNode* symbol[3];
+} TAC;
+
+
+#endif
